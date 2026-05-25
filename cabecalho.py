4ab309fa0318b27fdcd1db0e5ed5d4235ab9ec4a
MatheusSkywalkerKenobi <zeusmachado007@gmail.com>
2026-05-25 12:32:24 +0000

    v1

---
+++ cabecalho.py
@@ -0,0 +1,10 @@
+### Defininho e Chamando a Função
+def exibir_cabecalho():
+    print('=' * 30)
+    print('SISTEMA DE GESTÃO ESCOLAR')
+    print('Versão 1.0 - Desenvolvido no Codespaces')
+    print('=' * 30)
+
+### Chamando a função
+exibir_cabecalho ()
+print('Menu principal:')
---
+++ investimento_poup.py
@@ -0,0 +1,10 @@
+#Simulador de Investimento - Poupança --
+deposito = float(input("Digite o valor do aporte"))
+taxa = float(input("Qual a taxa da Poupança em %?"))
+meses = int(input("Quantos meses vai investir?"))
+conversao = taxa/100
+total = 0
+for mes in range(1,meses +1):
+    total = total + deposito
+    total = total + (total *taxa)
+print(f"Ao final do periodo, você tera: R${total:.2f}")
